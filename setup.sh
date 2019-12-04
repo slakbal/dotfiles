967854948c35d03607cef7605a02f29a74031bfa
Leslie Price <leslie.price78@gmai.com>
2019-12-04 17:07:09 +0200

    0

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ sudo -v
 #sudo add-apt-repository ppa:ondrej/PHP
 #sudo apt -y install php7.3-fpm
 #sudo apt -y install php-cli php-mbstring 
+#sudo apt -y install php7.3-common php7.3-mysql php7.3-xml php7.3-xmlrpc php7.3-curl php7.3-gd php7.3-imagick php7.3-cli php7.3-dev php7.3-imap php7.3-mbstring php7.3-opcache php7.3-soap php7.3-zip php7.3-intl
 echo "************ PHP installed *************"
 #php -v
 
