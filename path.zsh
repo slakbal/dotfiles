88e1149e57b7c107ce7db0529d3f7ce922164907
Slakbal <leslie.price78@gmail.com>
2021-01-12 10:13:58 +0200

    updated paths

--- path.zsh
+++ path.zsh
@@ -7,6 +7,9 @@ export PATH="$HOME/.node/bin:$PATH"
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
+# Use .local/bin also to path
+export PATH="$HOME/.local/bin:$PATH"
+
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
