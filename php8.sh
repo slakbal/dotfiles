cacbf83afbf243f8f121fc13507a4419e9b8d1af
Slakbal <leslie.price78@gmail.com>
2021-11-12 15:28:43 +0200

    Added Xdebug to php 8

--- php8.sh
+++ php8.sh
@@ -26,7 +26,7 @@ sudo apt install php8.0 -y
 #sudo apt install php8.0-cli php8.0-fpm php8.0-bcmath php8.0-curl php8.0-gd php8.0-intl php8.0-mbstring php8.0-mysql php8.0-opcache php8.0-sqlite3 php8.0-xml php8.0-zip -y
 
 
-sudo apt install php8.0-{common,gd,opcache,cli,bz2,fpm,curl,intl,bcmath,redis,mbstring,zip,mysql,readline,xml,sqlite3} php-sqlite3 openssl -y
+sudo apt install php8.0-{common,gd,opcache,cli,bz2,fpm,curl,intl,bcmath,redis,mbstring,zip,mysql,readline,xml,sqlite3,xdebug} php-sqlite3 openssl -y
 
 php -v
 
@@ -35,3 +35,16 @@ php -m
 php -m | grep sqlite
 
 #valet use
+#Activate XDEBUG
+# And configure it as:
+# sudo nano /etc/php/7.0/mods-available/xdebug.ini
+
+# Add the following code into it:
+# zend_extension=xdebug.so
+# xdebug.remote_autostart = 1
+# xdebug.remote_enable = 1
+# xdebug.remote_handler = dbgp
+# xdebug.remote_host = 127.0.0.1
+# xdebug.remote_log = /tmp/xdebug_remote.log
+# xdebug.remote_mode = req
+# xdebug.remote_port = 9005 #if you want to change the port you can change
